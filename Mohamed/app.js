658f28e1abf332333bc3f0630759882991d46cd5
Mohamed Koanda <m.koanda@aol.com>
2018-09-07 14:09:13 -0700

    few edited

--- Mohamed/app.js
+++ Mohamed/app.js
@@ -1,24 +1,22 @@
 const express = require('express');
 
-// create our app by invoking express
-const app = express();
-
-// app.HTTP_VERB('URL', function (req, res) { });  // HTTP_VERB is either 'get' or 'post' etc...
+// ********** START SETTING UP MY APP ********** //
+const app = express(); // create our app by invoking express
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // tell our app to use staic files
 app.use(express.static(__dirname + "/static"));
-app.use(express.static(__dirname + "/static/templates"));
-app.use(express.static(__dirname + "/static/css"));
-app.use(express.static(__dirname + "/static/images"));
 
 // tell our express app to use views
 app.set('views', __dirname + '/views');
-// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
 app.set('view engine', 'ejs');
+
+const port = 8000;
+// *************** FINISHED APP SETTING UP *************** //
+
+
 
 // app.get('/', function (req, res) { });
 
@@ -27,7 +25,7 @@ app.get('/cars', function (req, res) {
     res.render('cars');
 });
 
-// route for track and field
+// route for cats
 app.get('/cats', function (req, res) {
     res.render('cats');
 });
@@ -35,7 +33,6 @@ app.get('/cats', function (req, res) {
 app.get('/cars/new', function (req, res) {
     res.render('form');
 });
-
 
 const cats = require('./cats');
 // routes for cats details
@@ -61,14 +58,9 @@ app.post('/add_car', function (req, res) {
     res.redirect('/cars/new');
 });
 
-app.get("/users/:id", function (req, res) {
-    console.log("The user id requested is:", req.params.id);
-    // just to illustrate that req.params is usable here:
-    res.send("You requested the user with id: " + req.params.id);
-    // code to get user from db goes here, etc...
-});
 
 
-app.listen(8000, function () {
-    console.log("Server is running in port 8000");
+// ************* START SERVER AND LISTEN TO PORT *********** //
+app.listen(port, function () {
+    console.log("Server is running in port " + port);
 });
